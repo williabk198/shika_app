321bbe6a2fc416a26d0b0713bb2266d1d8672488
Tylor Lehman <tylorlehman@yahoo.com>
2018-04-21 18:09:24 -0400

    React native components of Shika app

---
+++ reactnative/src/config/data.js
@@ -0,0 +1,32 @@
+export const me = {
+    "gender": "male",
+    "name": {
+      "title": "mr",
+      "first": "Tylor",
+      "last": "Lehman"
+    },
+    "location": {
+      "street": "76 Hewittville Road",
+      "city": "Norwood",
+      "state": "New York",
+      "postcode": 13668
+    },
+    "pets":[
+        {"name":"Rowan","type":"dog","breed":"Poodle"},
+        {"name":"Luna","type":"dog","breed":"Husky"}
+    ],
+    "email": "tylorlehman@yahoo.com",
+    "login": {
+      "username": "tdlehman",
+    },
+    "dob": "1992-12-08",
+    "phone": "(585)-808-9369",
+    "cell": "(585)-808-9369",
+    "picture": {
+      "large": "https://randomuser.me/api/portraits/men/2.jpg",
+      "medium": "https://randomuser.me/api/portraits/med/men/2.jpg",
+      "thumbnail": "https://randomuser.me/api/portraits/thumb/men/2.jpg"
+    },
+    "nat": "US"
+  };
+  
---
+++ reactnative/src/login/loginScreen.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { View, Keyboard, Text, Input, Image, Dimensions, StyleSheet} from 'react-native'
+import {TextButton} from '../shared/ui'
+const dim = Dimensions.get('window')
+
+class LoginScreen extends Component{
+    render(){
+        return(
+            <View style={{flex:1, flexDirection:'column', backgroundColor:'black',alignItems:'center'}}>
+                <Image 
+                source={require('../images/DogParkTransparent.png')}
+                style={{height:dim.height/2}}resizeMode='contain'
+                />
+            <View style={{ width:dim.width,flexDirection:'row', justifyContent:'center'}}>
+                <TextButton 
+                    style={styles.buttonStyle} 
+                    onPress={()=>this.props.navigation.navigate('Home')}
+                >Log in with Facebook</TextButton>
+                </View>
+            </View>
+            )
+    }
+}
+const styles = StyleSheet.create({
+    buttonStyle:{
+        padding:50,
+        margin:50
+    }
+})
+export default LoginScreen
