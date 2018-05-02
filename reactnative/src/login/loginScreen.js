845d8abfea30d853d961402af126034984120c85
Tylor Lehman <TylorLehman@Frazer.biz>
2018-05-02 17:06:04 -0400

    MOre fb stuff

--- reactnative/src/login/loginScreen.js
+++ reactnative/src/login/loginScreen.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 import { View, Keyboard, Text, Input, Image, Dimensions, StyleSheet } from 'react-native'
 import { TextButton } from '../shared/ui'
-
+import { FBLogin } from 'react-native-facebook-login'
+import PropTypes from 'prop-types'
 const dim = Dimensions.get('window')
+var { FBLoginManager } = require('react-native-facebook-login')
+
+FBLoginManager.setLoginBehavior(FBLoginManager.LoginBehaviors.Web) // defaults to Native
 
 class LoginScreen extends Component {
+  _login () {
+    console.log('trying to login')
+    FBLoginManager.loginWithPermissions([ 'email', 'user_friends' ], function (
+      error,
+      data
+    ) {
+      if (!error) {
+        console.log('Login data: ', data)
+        this.props.navigation.navigate('Home')
+      } else {
+        console.log('Error: ', error)
+      }
+    })
+  }
+
   render () {
     return (
       <View
@@ -23,21 +42,20 @@ class LoginScreen extends Component {
         <View
           style={{ width: dim.width, flexDirection: 'row', justifyContent: 'center' }}
         >
-          <TextButton
-            style={styles.buttonStyle}
-            onPress={() => this.props.navigation.navigate('Home')}
-          >
-            Log in with Facebook
+          <TextButton style={styles.buttonStyle} onPress={() => this._login()}>
+            Sign in with Facebook
           </TextButton>
         </View>
       </View>
     )
   }
 }
+
 const styles = StyleSheet.create({
   buttonStyle: {
     padding: 50,
     margin: 50
   }
 })
+
 export default LoginScreen
--- reactnative/src/shared/ui/button.js
+++ reactnative/src/shared/ui/button.js
@@ -1,36 +1,46 @@
-
 import React from 'react'
 import { StyleSheet, TouchableWithoutFeedback } from 'react-native'
 import { Button, Text } from 'native-base'
 import PropTypes from 'prop-types'
 
-
-
-
-
-
 export const TextButton = ({ children, onPress, style }) => (
-    <Button onPress={onPress} style={{width:200,marginBottom:10, backgroundColor:'blue',justifyContent:'center'}} >
-      <Text style={{color:'white', fontWeight:'bold'}}>{children}</Text>
-    </Button>
-  )
-  TextButton.propTypes = {
-    children: PropTypes.any,
-    onPress: PropTypes.func.isRequired
-  }
-  TextButton.defaultProps = {
-    children: 'Click Me'
-  }
+  <Button
+    onPress={onPress}
+    style={{
+      width: 200,
+      marginBottom: 10,
+      backgroundColor: '#334477',
+      justifyContent: 'center'
+    }}
+  >
+    <Text style={{ color: 'white', fontWeight: 'bold' }}>{children}</Text>
+  </Button>
+)
+TextButton.propTypes = {
+  children: PropTypes.any,
+  onPress: PropTypes.func.isRequired
+}
+TextButton.defaultProps = {
+  children: 'Click Me'
+}
 
-  export const HomeButton = ({ children, onPress, style }) => (
-    <Button onPress={onPress} style={{width:200,marginBottom:10, backgroundColor:'yellow',justifyContent:'center'}} >
-      <Text style={{color:'black', fontWeight:'bold'}}>{children}</Text>
-    </Button>
-  )
-  TextButton.propTypes = {
-    children: PropTypes.any,
-    onPress: PropTypes.func.isRequired
-  }
-  TextButton.defaultProps = {
-    children: 'Click Me'
-  }
+export const HomeButton = ({ children, onPress, style }) => (
+  <Button
+    onPress={onPress}
+    style={{
+      width: 200,
+      marginBottom: 10,
+      backgroundColor: 'yellow',
+      justifyContent: 'center'
+    }}
+  >
+    <Text style={{ color: 'black', fontWeight: 'bold' }}>{children}</Text>
+  </Button>
+)
+TextButton.propTypes = {
+  children: PropTypes.any,
+  onPress: PropTypes.func.isRequired
+}
+TextButton.defaultProps = {
+  children: 'Click Me'
+}
