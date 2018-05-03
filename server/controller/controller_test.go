79ce92f8ce9e047b1033860fadaba961915a8044
Brandon Williams <bkwilliams92@yahoo.com>
2018-05-02 23:36:45 -0400

    Fixed mistake in controller test.
    The controller test was using the actual API key of the phone client
    app. Dummy test info is now being used in its stead as it should have
    been from the start.

--- server/controller/controller_test.go
+++ server/controller/controller_test.go
@@ -4,7 +4,18 @@ import "testing"
 
 func TestAPICheck(t *testing.T) {
 
-	err := checkAPIKey("shika", "0sdfgjZLKJw459klsd345kjjLKJf956B")
+	testApp := struct {
+		Name string
+		Key  string
+	}{
+		"test",
+		"b6d637a20cc423665dc49b5c2bd008c28c1f2431d4f45f6ab4241ca2df5bec29",
+	}
+
+	creds.AllowedApps = append(creds.AllowedApps, testApp)
+	creds.Salt = "76543210"
+
+	err := checkAPIKey("test", "01234567")
 	if err == nil {
 		t.Error("Expected an error but did not get one")
 	}
@@ -12,7 +23,7 @@ func TestAPICheck(t *testing.T) {
 		t.Errorf("Expected an errBadKey error but got another error\n%v", err)
 	}
 
-	err = checkAPIKey("bad_app", "M53BdwLOG0hlNJXK7cQpkSWOYMlC5UKW")
+	err = checkAPIKey("bad_app", "12345678")
 	if err == nil {
 		t.Error("Expected an error but did not get one")
 	}
@@ -20,7 +31,7 @@ func TestAPICheck(t *testing.T) {
 		t.Errorf("Expected an errAppDisallowed error but got another error\n%v", err)
 	}
 
-	err = checkAPIKey("shika", "M53BdwLOG0hlNJXK7cQpkSWOYMlC5UKW")
+	err = checkAPIKey("test", "12345678")
 	if err != nil {
 		t.Errorf("Got an error but was not expecing one\n%v", err)
 	}
